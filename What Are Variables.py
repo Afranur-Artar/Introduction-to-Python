a5871b0f3568efe45819dc5bf930cf678eb2d3d1
Afranur-Artar <69244480+Afranur-Artar@users.noreply.github.com>
2020-08-06 21:30:59 +0300

    Add files via upload

---
+++ What Are Variables.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Soru 1:
+# 
+# Define two variables as x and y. Assign 1 to x and '1' to y. Then check if two variables have the same value and type.
+
+# In[4]:
+
+
+#Cevap 1:
+
+x=1
+y='1'
+
+x==y
+1=='1'
+
+
+# # Soru 2:
+# 
+# Change the type of variable y and make the two variables the same.
+
+# In[12]:
+
+
+#Cevap 2:
+
+x=1
+y='1'
+
+del y
+y=1
+x==y
+
+
+# In[ ]:
+
+
+
+
