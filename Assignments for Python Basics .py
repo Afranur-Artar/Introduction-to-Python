3c8556d9915ed5225c2c53e9d4a9edc4331ac510
Afranur-Artar <69244480+Afranur-Artar@users.noreply.github.com>
2020-08-06 16:33:07 +0300

    Add files via upload

---
+++ Assignments for Python Basics .py
@@ -0,0 +1,16 @@
+#Homework1 and 2
+print("Afranur Artar")
+print("Afranur\n Artar")
+#or
+print("\nAfranur","Artar",sep="\n")
+
+#print içerisine yazılan değerleri alt satırlarda göstermek için 2 farklı yöntem kullanılır:
+#Ya ""  tırnak içerisine yazılan değerlerin arasına \n koyulur;
+#Ya da ayrı ayrı yazılacak her değer print içerisinde ayrı "" lar ile gösterilir ve ,sep="\n"  eklenir.
+
+#Homework 3:
+#print içerisine yazılan değer alt satırdan devam edecekse 3 tırnak kullanılır
+#print içerisinde bir alıntı daha yapılacaksa dıştaki print için
+#tek tırnak kullanılır.
+
+print("I don't want to be an 'artist'. I want to be a 'Data Scientist'")
